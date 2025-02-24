9a8d05705cf4bae2b47149f6f293df53370d8731
Jacobwkidd <kiddj1@students.westerntc.edu>
2025-02-23 19:55:19 -0600

    I got PostgreSQL connected!

--- Stock Market App/stock-market-backend/src/app.module.ts
+++ Stock Market App/stock-market-backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StocksModule } from './stocks/stocks.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from 'config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), // Load environment variables globally
-    StocksModule,],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), 
+            StocksModule, TypeOrmModule.forRoot(databaseConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
